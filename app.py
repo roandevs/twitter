14bf2238f5b202ebe165e0b43694eedb02910e6f
roan <roanj200@gmail.com>
2023-03-07 23:42:15 +0000

    add log with gunicorn option

--- app.py
+++ app.py
@@ -3,14 +3,25 @@ from flask_cors import CORS
 from routes.accounts import accounts
 from routes.tweets import tweets
 from utils.sentiment_analysis import train
+import logging
 
 app = Flask(__name__)
 CORS(app)
+LOG_WITH_GUNICORN=True
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+def start_logging():
+    if LOG_WITH_GUNICORN:
+        gunicorn_error_logger = logging.getLogger('gunicorn.error')
+        app.logger.handlers.extend(gunicorn_error_logger.handlers)
+        app.logger.setLevel(logging.DEBUG)
+        
+
+print('Running logger')
+start_logging()
 print('Starting training process')
 train()
 print('Registering routes')
