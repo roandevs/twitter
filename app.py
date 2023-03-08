ea03fe8eb16e19a5ad4467c564b6c8bfc4411561
Roan John <112395163+roandevs@users.noreply.github.com>
2023-03-08 01:44:51 +0000

    Refactor app.py to run Flask natively

--- app.py
+++ app.py
@@ -3,29 +3,18 @@ from flask_cors import CORS
 from routes.accounts import accounts
 from routes.tweets import tweets
 from utils.sentiment_analysis import train
-import logging
 
 app = Flask(__name__)
 CORS(app)
-LOG_WITH_GUNICORN=True
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-def start_logging():
-    if LOG_WITH_GUNICORN:
-        gunicorn_error_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers.extend(gunicorn_error_logger.handlers)
-        app.logger.setLevel(logging.DEBUG)
-        
-
-
-start_logging()
-logging.info('Starting training process')
-train()
-logging.info('Registering routes')
-app.register_blueprint(accounts)
-app.register_blueprint(tweets)
-    
-
+if __name__ == "__main__":
+    print('Starting training')
+    train()
+    print('Registering routes')
+    app.register_blueprint(accounts)
+    app.register_blueprint(tweets)
+    app.run(host="0.0.0.0", port=8080)
